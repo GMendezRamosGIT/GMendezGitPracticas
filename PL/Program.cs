8ba4a336d4c524f455af90e51024fdf8d6aa672f
Francisco Batalla <falfonsob01@outlook.com>
2026-01-14 13:44:41 -0600

    Se manda commit con 2 mensajes en consola

--- PL/Program.cs
+++ PL/Program.cs
@@ -11,6 +11,7 @@ namespace PL
         static void Main(string[] args)
         {
             Console.WriteLine("Se crea una rama llamada 'practica'");
+            Console.WriteLine("Se manda commit en rama");
         }
     }
 }
